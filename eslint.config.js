60bfb68b410326ebf10eede35bad713e70e06096
Lucaus <mzx@go-goal.com>
2024-08-15 19:38:33 +0800

    feat: ✨ 添加了 prettier 和 less，增加了文件夹快捷路径

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config({
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ["@myHooks", "./src/hooks"],
+          ["@myComponents", "./src/components"],
+          ["@myUtils", "./src/utils"],
+          ["@myStore", "./src/store"],
+          ["@myTypes", "./src/types"],
+          ["@myPages", "./src/pages"],
+          ["@myAssets", "./src/assets"],
+          ["@myContants", "./src/constants"],
+        ]
+      }
+    }
+  }
+})
